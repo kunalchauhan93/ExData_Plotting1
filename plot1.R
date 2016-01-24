29a74a02812c83cba719fc5ff4822668c00971b4
kunalchauhan93 <kunalchauhan9@gmail.com>
2016-01-24 16:42:44 +0530

    Course Project 1 - Initial Commit after Fork

---
+++ plot1.R
@@ -0,0 +1,9 @@
+dFile <- "./data/household_power_consumption.txt"
+data <- read.table(dFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subsetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subsetData)
+globalActivePower <- as.numeric(subsetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
